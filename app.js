356e03f922c913f6f57a4835b66193b0b1f5a5f3
ishara18 <32064390+ishara18@users.noreply.github.com>
2020-04-10 16:38:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#add_details');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('maincollection').add({
+        name : form.name.value,
+        email : form.email.value,
+        subject : form.subject.value,
+        description : form.description.value      
+    });
+    form.name.value = '';
+    form.email.value = '';
+    form.subject.value = '';
+    form.escription.value = '';
+    thank();
+})
+
+function thank()
+{
+    alert("Thank You");
+}
